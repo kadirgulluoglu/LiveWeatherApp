7a9cad658e428e92690bce3f8671441116339891
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-11 22:44:51 +0300

    create weather api with http

---
+++ lib/api/weather_api.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/models/weather_model.dart';
+import 'package:geolocator/geolocator.dart';
+
+class WeatherData {
+  Future<Weather> getData(var latitude, var longitude) async {
+    Uri uri = Uri.parse(
+        "http://api.weatherapi.com/v1/current.json?key=0848b6375b0c44af951181922221108&q=$latitude,$longitude&aqi=no");
+    var response = await http.get(uri);
+    var body = jsonDecode(response.body);
+    return Weather.fromJson(body);
+  }
+}
